a610f2938fd66e3635f4c6e475f56b6b37a7a472
EREN <EREN@DESKTOP-DRT5BB2>
2022-11-01 02:08:25 +0300

    xxx

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require('path');
 const validator = require('email-validator');
 const nodemailer = require('nodemailer');
 const hbs = require('nodemailer-express-handlebars');
+const { promisify } = require('util');
+const request = promisify(require('request'));
 
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
@@ -14,13 +17,35 @@ app.get('/', (req, res) => {
     res.render('main', { title: 'Hey', message: 'Hello there!' });
 });
 
-app.post("/", (req, res) => {
+// app.post("/faq", async (req, res) => {
+//     console.log(req.body);
+//     const name = req.body.name;
+//     const response_key = req.body["g-recaptcha-response"];
+//     const secret_key = "6LcND8wiAAAAAJhbeYXn95IgauLHxCeUTtCPuwkR";
+//     const options = {
+//         url: `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`,
+//         headers: { "Content-Type": "application/x-www-form-urlencoded", 'json': true },
+//     };
+//     try {
+//         const re = await request(options);
+//         console.log(options);
+//         if (!JSON.parse(re.body)['success']) {
+//             return res.send({ response: "Failed" });
+//         }
+//         return res.send({ response: "Successful" });
+//     } catch (error) {
+//         return res.send({ response: "Failed" });
+//     }
+// });
+
+
+app.post("/faq", async (req, res) => {
     console.log(req.body);
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'demireleren1903@gmail.com',
-            pass: 'tumzbzyvjmfvtfao'
+            user: 'softhub.iletisim1@gmail.com',
+            pass: 'vnrbhdxoqbcnlzvn'
         }
     });
 
@@ -46,22 +71,44 @@ app.post("/", (req, res) => {
         },
         from: req.body.email,
         to: "demireleren877@gmail.com",
-        subject: 'New Message From' + req.body.email + ': ' + req.body.subject,
-        text: req.body.message
+        subject: 'New Message From softhub.com',
     };
+    const response_key = req.body["g-recaptcha-response"];
+    const secret_key = "6LcND8wiAAAAAJhbeYXn95IgauLHxCeUTtCPuwkR";
+    const options = {
+        url: `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`,
+        headers: { "Content-Type": "application/x-www-form-urlencoded", 'json': true },
+    };
+
 
     if (req.body.name && req.body.email && req.body.subject && req.body.message) {
         if (validator.validate(req.body.email)) {
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                    res.send('error');
-                } else {
-                    console.log('Email sent: ' + info.response);
-                    res.send('success');
+            console.log("Email is valid");
+            try {
+                const re = await request(options);
+                console.log(options);
+                if (!JSON.parse(re.body)['success']) {
+                    return res.send({ response: "Failed" });
                 }
-            });
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                        res.send('error');
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                        res.send('success');
+                    }
+                });
+                return res.redirect('/');
+            } catch (error) {
+                return res.send({ response: "Failed" });
+            }
+
+
         }
+    } else {
+        res.send('error');
     }
 
 
